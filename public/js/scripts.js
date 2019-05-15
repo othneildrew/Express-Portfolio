be4389cc129d53a73e5dc5edf34e29cb98d9150b
Othneil Drew <codeguydrew@gmail.com>
2019-05-15 11:47:56 -0500

    Added feature to show animated gif on project thumbnail hover

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,12 @@
 $(document).foundation()
+
+// Show animated photo on project thumbnail hover
+$('.thumbnail')
+  .mouseenter(function() {
+    let animatedSrc = $(this).attr('src').replace('.png', '.gif');
+    $(this).attr('src', animatedSrc);
+  })
+  .mouseleave(function() {
+    let mainSrc = $(this).attr('src').replace('.gif', '.png');
+    $(this).attr('src', mainSrc);
+  });
